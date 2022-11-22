64475d8b2d679f13b4576244d4cdb10a41c9508a
winvairu <winvairu@outlook.com>
2022-11-22 22:32:39 +0300

    modified task_3/task_3.cs

--- lesson_1_introduction_to_the_C#_programming_language/task_3/task_3.cs
+++ lesson_1_introduction_to_the_C#_programming_language/task_3/task_3.cs
@@ -1,2 +1,39 @@
 ﻿/* Write a program that will give out the name of the day of the week by a given number.*/
 
+Console.WriteLine("Enter a number indicating the day of the week: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Your day is Monday");
+        break;
+
+    case 2:
+        Console.WriteLine("Your day is Tuesday");
+        break;
+
+    case 3:
+        Console.WriteLine("Your day is Wednesday");
+        break;
+
+    case 4:
+        Console.WriteLine("Your day is Thursday");
+        break;
+
+    case 5:
+        Console.WriteLine("Your day is Friday");
+        break;
+
+    case 6:
+        Console.WriteLine("Your day is Saturday");
+        break;
+
+    case 7:
+        Console.WriteLine("Your day is Sunday");
+        break;
+
+    default:
+        Console.WriteLine("Incorrect data");
+        break;
+}
