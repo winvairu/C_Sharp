126628f6a3059fde7ecd0970eef4cb9be44f8291
winvairu <winvairu@outlook.com>
2022-11-23 01:15:18 +0300

    add task_8

---
+++ lesson_1_introduction_to_the_C#_programming_language/task_8/task_8.cs
@@ -0,0 +1,15 @@
+﻿/* Write a program that takes a number (N) as input,
+and shows all even numbers from 1 to N as output. */
+
+Console.WriteLine("Enter an unsigned integer:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < n; i++)
+{
+    if (i % 2 == 0)
+    Console.WriteLine(i);
+}
+if (n < 1)
+{
+    Console.WriteLine("the number must be greater than 1");
+}
