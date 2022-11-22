57e80f944e4a00200c2cb961eea0c2ab210cd5ce
winvairu <winvairu@outlook.com>
2022-11-22 23:19:29 +0300

    add task_4

---
+++ lesson_1_introduction_to_the_C#_programming_language/task_4/task_4.cs
@@ -0,0 +1,19 @@
+﻿/* Write a program that accepts three numbers as input and outputs the maximum of these numbers. */
+
+Console.WriteLine("Enter the first integer:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second integer:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the third integer:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > a) 
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine($"The maximum number is: {max}");
