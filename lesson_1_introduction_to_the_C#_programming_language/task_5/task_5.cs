76913f5e3656fbf3f859e89842fbb2d472a4c875
winvairu <winvairu@outlook.com>
2022-11-23 00:39:42 +0300

    add task_5

---
+++ lesson_1_introduction_to_the_C#_programming_language/task_5/task_5.cs
@@ -0,0 +1,10 @@
+﻿/* Write a program that takes one unsigned integer (N) as input,
+and shows all integers in the range from -N to N. */
+
+Console.WriteLine("Enter an unsigned integer:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int b = n * (-1); b <= n; b++)
+{
+    Console.WriteLine(b);
+}
