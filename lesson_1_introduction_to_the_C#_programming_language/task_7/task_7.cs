8661a1229a5e11718d4923145c9a9b5902d3ace8
winvairu <winvairu@outlook.com>
2022-11-23 00:59:36 +0300

    add task_7

---
+++ lesson_1_introduction_to_the_C#_programming_language/task_7/task_7.cs
@@ -0,0 +1,8 @@
+﻿/* Write a program that accepts a three-digit integer as input and
+shows the last digit of this number as output. */
+
+Console.WriteLine("Enter a three-digit integer:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+a = a % 10;
+Console.WriteLine($"The last digit is equal to: {a}");
