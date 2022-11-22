2cf26c471d3f421dcf1b5dad8c31098630258d48
winvairu <winvairu@outlook.com>
2022-11-23 00:51:20 +0300

    add task_6

---
+++ lesson_1_introduction_to_the_C#_programming_language/task_6/task_6.cs
@@ -0,0 +1,13 @@
+﻿/* Write a program that takes a number as input and outputs whether the number is even. */
+
+Console.WriteLine("Enter an integer:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.WriteLine($"{n} an even number");
+}
+else
+{
+    Console.WriteLine($"{n} not an even number");
+}
