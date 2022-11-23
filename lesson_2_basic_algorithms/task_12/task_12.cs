c0342be9ccc53a54fa511244b95393d4c0984d01
winvairu <winvairu@outlook.com>
2022-11-24 02:53:03 +0300

    add task_12

---
+++ lesson_2_basic_algorithms/task_12/task_12.cs
@@ -0,0 +1,18 @@
+﻿/* Write a program that will take two numbers as input and
+output whether the second number is a multiple of the first.
+If the second number is not a multiple of the first,
+then the program outputs the remainder of the division. */
+
+Console.WriteLine("Enter the first integer: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second integer: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine($"{b} is a multiple of {a}");
+}
+else
+{
+    Console.WriteLine($"{b} is not a multiple of {a}, the remainder is {a % b}");
+}
