2761d8989a2558a07d4b20a81f61eb1147a7a570
winvairu <winvairu@outlook.com>
2022-11-23 23:06:14 +0300

    add task_10

---
+++ lesson_2_basic_algorithms/task_10/task_10.cs
@@ -0,0 +1,23 @@
+﻿/* Write a program that accepts a three-digit number as input and
+shows the second digit of this number as output. */
+
+Console.WriteLine("Enter a three-digit integer:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Abs(a).ToString().Length == 3)
+{
+    if (a > 0)
+    {
+    a = (a % 100) / 10;
+    Console.WriteLine($"The number {a} is in the middle.");
+    }
+    else
+    {
+        a = (a % 100) / 10 * (-1);
+    Console.WriteLine($"The number {a} is in the middle.");
+    }
+}
+else
+{
+    Console.WriteLine($"Invalid number entered.");
+}
