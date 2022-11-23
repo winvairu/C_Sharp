d9cfef83e46124d83cc511a10da71ed13d5ed576
winvairu <winvairu@outlook.com>
2022-11-23 23:32:10 +0300

    add task_11

---
+++ lesson_2_basic_algorithms/task_11/task_11.cs
@@ -0,0 +1,9 @@
+﻿/* Write a program that outputs a random three-digit number and
+deletes the second digit of that number. */
+
+int a = new Random().Next(100, 999);
+Console.WriteLine($"{a}");
+int b = a / 100;
+int c = a % 10;
+a = b * 10 + c;
+Console.WriteLine($"{a}");
