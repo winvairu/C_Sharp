a884317281e72cc4cae75817f46658b47389ba33
winvairu <winvairu@outlook.com>
2022-11-24 17:46:11 +0300

    add task_14

---
+++ lesson_2_basic_algorithms/task_14/task_14.cs
@@ -0,0 +1,16 @@
+﻿/* Write a program that takes a number as input and
+checks whether it is a multiple of 7 and 23 at the same time. */
+
+int a = 7;
+int b = 23;
+Console.WriteLine("Enter an integer:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if ((c % a == 0) && (c % b == 0))
+{
+    Console.WriteLine($"{c} - multiple number");
+}
+else
+{
+    Console.WriteLine($"{c} - not a multiple number");
+}
