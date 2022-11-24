592609546505be1ba0b9a2a55ad515785f6c95c4
winvairu <winvairu@outlook.com>
2022-11-24 16:36:28 +0300

    add task-13

---
+++ lesson_2_basic_algorithms/task_13/task_13.cs
@@ -0,0 +1,29 @@
+﻿/* Write a program that outputs the third digit of a given number or
+reports that there is no third digit. */
+
+Console.WriteLine("Enter a three-digit integer:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 0)
+{
+    a = a * (-1);
+}
+int b = a / 100;
+
+if (b >= 1)
+{
+    int temp = a;
+    int index = 0;
+    while (temp > 0)
+    {
+        temp = temp / 10;
+        index++;
+    }
+    int power = index - 3;
+    int c = Convert.ToInt32(Math.Pow(10, power));
+    int d = (a / c) % 10;
+    Console.WriteLine($"The third number: {d}");
+}
+else
+{
+    Console.WriteLine("There is no third digit.");
+}
