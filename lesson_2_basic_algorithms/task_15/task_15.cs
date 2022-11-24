65a7920901705c82d451109d55805617b3b26466
winvairu <winvairu@outlook.com>
2022-11-24 18:05:35 +0300

    add task_15

---
+++ lesson_2_basic_algorithms/task_15/task_15.cs
@@ -0,0 +1,16 @@
+﻿/* Write a program that accepts a digit indicating the day
+of the week as input and checks whether this day is a weekend. */
+
+Console.WriteLine("Enter a number indicating the day of the week: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 5 && a <= 7)
+{
+    Console.WriteLine("You have chosen a day off.");
+} else if ( a >= 1 && a <= 5)
+        {
+            Console.WriteLine("You have chosen a working day.");
+        }
+else
+{
+    Console.WriteLine("The number does not correspond to the day of the week.");
+}
