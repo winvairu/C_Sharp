3f8f31079b3108caacc8dcb618c1c514b822322b
winvairu <winvairu@outlook.com>
2022-11-29 03:47:05 +0300

    add task_18

---
+++ lesson_3_basic_algorithms_continuation/task_18/task_18.cs
@@ -0,0 +1,39 @@
+﻿/* Write a program that, according to a given number of quarters,
+shows the range of possible coordinates of points in this quarter (X and Y). */
+
+Console.WriteLine("Enter the number of the coordinate quarter (1-4):");
+int z = Convert.ToInt32(Console.ReadLine());
+int x, y;
+
+if (z == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+    x = new Random().Next(1, 999);
+    y = new Random().Next(1, 999);
+    Console.WriteLine($"Example: x = {x}, y = {y}");
+}
+else if (z == 2)
+{
+    Console.WriteLine("x > 0, y < 0");
+    x = new Random().Next(1, 999);
+    y = new Random().Next(1, 999);
+    Console.WriteLine($"Example: x = {x}, y = {y}");
+}
+else if (z == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+    x = new Random().Next(1, 999);
+    y = new Random().Next(1, 999);
+    Console.WriteLine($"Example: x = {x}, y = {y}");
+}
+else if (z == 4)
+{
+    Console.WriteLine("x < 0, y > 0");
+    x = new Random().Next(1, 999);
+    y = new Random().Next(1, 999);
+    Console.WriteLine($"Example: x = {x}, y = {y}");
+}
+else
+{
+    Console.WriteLine("Wrong quarter.");
+}
