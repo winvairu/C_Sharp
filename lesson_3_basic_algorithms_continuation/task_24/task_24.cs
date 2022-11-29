5ec1d564614ccf7feadafdc83474f5d3db74f875
winvairu <winvairu@outlook.com>
2022-11-29 22:26:25 +0300

    add task_24

---
+++ lesson_3_basic_algorithms_continuation/task_24/task_24.cs
@@ -0,0 +1,25 @@
+﻿/* Set N and print "#" to N with a herringbone. */
+
+Console.WriteLine("Enter an odd unsigned integer (number ≥ 3):");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ((n >= 3) && ((n % 2) != 0))
+{
+for (int i = 1, a = n/2; i <= n; i += 2, a--)
+{
+    for (int j = a; j > 0; j--)
+    {
+        Console.Write(" ");
+    }
+    for (int m = 0; m < i; m++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Happy New Year!");
+}
+else
+{
+    Console.WriteLine("The number does not meet the conditions.");
+}
