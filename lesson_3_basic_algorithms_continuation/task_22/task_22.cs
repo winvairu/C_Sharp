c52086a57c935bbc32be117a15c85dba9b21edcb
winvairu <winvairu@outlook.com>
2022-11-29 16:37:38 +0300

    add task_22

---
+++ lesson_3_basic_algorithms_continuation/task_22/task_22.cs
@@ -0,0 +1,18 @@
+﻿/* Divide the watermelon for Petya and Vasya so that the
+share of each weighs exactly an even number of kilograms,
+while it is not necessary that the shares are equal in size.
+The weight of the watermelon is 10 kilograms. */
+
+int weight = 10;
+int forPetya;
+int forVasya;
+
+for (forPetya = 1; forPetya < 10; forPetya++)
+{
+    if (forPetya % 2 == 0)
+    {
+        forVasya = weight - forPetya;
+        Console.WriteLine("Sizes of watermelon pieces for Petya and Vasya:");
+        Console.WriteLine($"{forPetya}, {forVasya};");
+    }
+}
