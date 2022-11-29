32a641ca3b3ee3d397a0f5b23b87082449e10340
winvairu <winvairu@outlook.com>
2022-11-29 04:35:44 +0300

    add task_19

---
+++ lesson_3_basic_algorithms_continuation/task_19/task_19.cs
@@ -0,0 +1,26 @@
+﻿/* Write a program that accepts a five-digit number as input and
+checks whether it is a palindrome. */
+
+Console.WriteLine("Enter an unsigned five-digit integer:");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = x / 10000;
+
+if (y >= 1 && y <= 9)
+{
+    int a1 = y;
+    int a5 = x % 10;
+    int a2 = (x / 1000) % 10;
+    int a4 = (x / 10) % 10;
+    if ((a1 == a5) && (a2 == a4))
+    {
+        Console.WriteLine($"The number {x} is a palindrome.");
+    }
+    else
+    {
+        Console.WriteLine($"The number {x} is not a palindrome.");
+    }
+}
+else
+{
+    Console.WriteLine("This number is not a five-digit number or it is signed.");
+}
