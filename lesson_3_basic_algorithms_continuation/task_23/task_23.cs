fbf1afc2b6675ca99f87f1fe2f81648ffbd0e33c
winvairu <winvairu@outlook.com>
2022-11-29 16:55:26 +0300

    add task_23

---
+++ lesson_3_basic_algorithms_continuation/task_23/task_23.cs
@@ -0,0 +1,11 @@
+﻿/* Write a program that takes a number (N) as input and
+outputs a table of cubes of numbers from 1 to N. */
+
+Console.WriteLine("Enter an unsigned integer (n ≥ 1):");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Cube of numbers from 1 to {n}:");
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"{i * i * i}");
+}
