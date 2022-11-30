2ef259d35b98149cbddce91320b3ea911c3523d0
winvairu <winvairu@outlook.com>
2022-11-30 18:54:33 +0300

    add task_27

---
+++ lesson_4_functions/task_27/task_27.cs
@@ -0,0 +1,19 @@
+﻿/* Write a program that accepts a number as input and
+outputs the sum of the digits in the number. */
+
+using static System.Console;
+
+int sumDigits(int a)
+{
+    int sum = 0;
+    for (int n = a * 10, remains = 0; n > 0; n = n / 10)
+    {
+        remains = n % 10;
+        sum += remains;
+    }
+    return sum;
+}
+
+WriteLine("Enter an unsigned integer:");
+int x = Convert.ToInt32(ReadLine());
+WriteLine($"Sum of all digits: {sumDigits(x)}");
