0801eb57ce3644e28c72aff23b8f1e5b02d79022
winvairu <winvairu@outlook.com>
2022-11-30 14:35:47 +0300

    del task_25

--- lesson_3_basic_algorithms_continuation/task_25/task_25.cs
+++
@@ -1,8 +0,0 @@
-﻿/* We have a cash - N $, we want to keep it from inflation,
-we put it in the bank and get M% once a year,
-after how many years we will be able to buy an apartment for K $,
-despite the fact that the price of the apartment also increases by Q%
-if this percentage is greater than or equal to N,
-then withdraw to the console is hopeless. */
-
-Console.WriteLine("The task has many inaccuracies.");
