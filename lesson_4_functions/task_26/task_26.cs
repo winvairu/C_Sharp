767a7ffe9741db90f006f693d85c55d1ee986fdd
winvairu <winvairu@outlook.com>
2022-12-06 03:31:25 +0300

    add task_26

---
+++ lesson_4_functions/task_26/task_26.cs
@@ -0,0 +1,41 @@
+﻿/* Write a program that accepts a number (A) as input and
+outputs the sum of numbers from 1 to A. */
+
+using static System.Console;
+
+int sumDigNum(int a)
+{
+    int sum = 0;
+   for (; a > 0; a--)
+   {
+    sum +=a;
+   }
+   return sum;
+}
+
+int num = 0;
+while (true)
+{
+    Write("Enter an unsigned integer or \"0\" to complete: ");
+    if (int.TryParse(ReadLine(), out num))
+    {
+        if (num == 0)
+        {
+            WriteLine($"End");
+            break;
+        }
+        else if (num < 0)
+        {
+            WriteLine("Invalid input.");
+        }
+        else
+        {
+            int s = sumDigNum(num);
+            WriteLine($"{s}");
+        }
+    }
+    else
+    {
+        WriteLine("Invalid input.");
+    }
+}
