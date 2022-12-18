4c926e9ad31f2d6d7c24553f6ca09cc4db4a02c1
winvairu <winvairu@outlook.com>
2022-12-18 16:33:08 +0300

    add task 32

---
+++ lesson_5_functions_and_one-dimensional_arrays/task_32/task_32.cs
@@ -0,0 +1,51 @@
+﻿/* Write a program replacing array elements: replace the positive elements with the
+corresponding negative ones, and vice versa. */
+
+using static System.Console;
+
+int[] createArray(int a)
+{
+    
+    int[] array = new int[a];
+    int[] array2 = new int[a]; 
+    for (int i = 0; a > 0; a--, i++)
+    {
+        array[i] = new Random().Next(-99, 99);
+        Write(array[i]);
+        Write(" ");
+        array2[i] = array[i] * (-1);
+    }
+    WriteLine();
+    return array2;
+}
+int num;
+while (true)
+{
+    Write("Enter the number of array elements or \"0\" to complete: ");
+    if (int.TryParse(ReadLine(), out num))
+    {
+        if (num == 0)
+        {
+            WriteLine("End");
+            break;
+        }
+        else if (num < 0)
+        {
+            WriteLine("Invalid input.");
+        }
+        else
+        {
+            int[] t = createArray(num);
+            for (int j = 0; j < num; j++)
+            {
+                Write(t[j]);
+                Write(" ");
+            }
+            WriteLine();
+        }
+    }
+    else
+    {
+        WriteLine("Invalid input.");
+    }
+}
