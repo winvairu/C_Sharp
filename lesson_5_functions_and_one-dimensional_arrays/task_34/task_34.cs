35e71510eb4775d55108b822a473f4d1527e2193
winvairu <winvairu@outlook.com>
2022-12-18 18:36:11 +0300

    add task 34

---
+++ lesson_5_functions_and_one-dimensional_arrays/task_34/task_34.cs
@@ -0,0 +1,49 @@
+﻿/* Set an array filled with random positive three-digit numbers.
+Write a program that will show the number of even numbers in the array. */
+
+using static System.Console;
+
+int createArray(int a)
+{
+    int evenNumbers = 0;
+    int[] array = new int[a];
+    for (int i = 0; a > 0; a--, i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        Write(array[i]);
+        Write(" ");
+        if (array[i] % 2 == 0)
+        {
+            evenNumbers += 1;
+        }
+    }
+    WriteLine();
+    return evenNumbers;
+}
+
+int num;
+while (true)
+{
+    Write("Enter the number of array elements or \"0\" to complete: ");
+    if (int.TryParse(ReadLine(), out num))
+    {
+        if (num == 0)
+        {
+            WriteLine("End");
+            break;
+        }
+        else if (num < 0)
+        {
+            WriteLine("Invalid input.");
+        }
+        else
+        {
+            WriteLine($"The number of even numbers in the array: {createArray(num)}");
+            WriteLine();
+        }
+    }
+    else
+    {
+        WriteLine("Invalid input.");
+    }
+}
