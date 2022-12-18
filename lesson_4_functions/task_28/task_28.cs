b0aeaba7e8fe1300f377a7f8d8c3a2c14f45b311
winvairu <winvairu@outlook.com>
2022-12-18 09:49:30 +0300

    add task 28

---
+++ lesson_4_functions/task_28/task_28.cs
@@ -0,0 +1,41 @@
+﻿/* Write a program that accepts a number as input and
+outputs the number of digits in it. */
+
+using static System.Console;
+
+int digitCounter(int a)
+{
+    int sum = 0;
+    for (int i = 0; a > 0; i++)
+    {
+        a /= 10;
+        sum += 1;
+    }
+    return sum;
+}
+int num = 0;
+while (true)
+{
+    Write("Enter an unsigned integer or \"0\" to complete: ");
+    if (int.TryParse(ReadLine(), out num))
+    {
+        if (num == 0)
+        {
+            WriteLine("End");
+            break;
+        }
+        else if (num < 0)
+        {
+            WriteLine("Invalid input.");
+        }
+        else
+        {
+            int s = digitCounter(num);
+            WriteLine($"{s}");
+        }
+    }
+    else
+    {
+        WriteLine("Invalid input.");
+    }
+}
