88321e1571296d9834d087b9f3fcd11d296ea4a6
winvairu <winvairu@outlook.com>
2022-12-18 19:14:57 +0300

    add task 36

---
+++ lesson_5_functions_and_one-dimensional_arrays/task_36/task_36.cs
@@ -0,0 +1,49 @@
+﻿/* Set a one-dimensional array filled with random numbers.
+Find the sum of the elements standing in odd positions. */
+
+using static System.Console;
+
+int createArray(int a)
+{
+    int sum = 0;
+    int[] array = new int[a];
+    for (int i = 0; a > 0; a--, i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+        Write(array[i]);
+        Write(" ");
+        if (i % 2 > 0)
+        {
+            sum += array[i];
+        }
+    }
+    WriteLine();
+    return sum;
+}
+
+int num;
+while (true)
+{
+    Write("Enter the number of array elements or \"0\" to complete: ");
+    if (int.TryParse(ReadLine(), out num))
+    {
+        if (num == 0)
+        {
+            WriteLine("End");
+            break;
+        }
+        else if (num < 0)
+        {
+            WriteLine("Invalid input.");
+        }
+        else
+        {
+            WriteLine($"The sum of the elements in odd positions.: {createArray(num)}");
+            WriteLine();
+        }
+    }
+    else
+    {
+        WriteLine("Invalid input.");
+    }
+}
