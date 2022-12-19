cc90559cd384e6754a82620db054f18b4d232397
winvairu <winvairu@outlook.com>
2022-12-19 17:38:21 +0300

    add task 41

---
+++ lesson_6_one-dimensional_arrays_continuation/task_41/task_41.cs
@@ -0,0 +1,60 @@
+﻿/* The user enters M numbers from the keyboard.
+Count how many numbers greater than 0 the user entered. */
+
+using static System.Console;
+
+int[] createArray(int a)
+{
+    int[] array = new int[a];
+    for (int i = 0; a > 0; a--, i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+        Write(array[i]);
+        Write(" ");
+    }
+    WriteLine();
+
+    return array;
+}
+
+int numberCounter(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+int num = 0;
+while (true)
+{
+    Write("Enter the number of elements of the integer array or enter \"0\" to exit: ");
+    if (int.TryParse(ReadLine(), out num))
+    {
+        if (num == 0)
+        {
+            WriteLine("End");
+            break;
+        }
+        else if (num < 0)
+        {
+            WriteLine("Invalid input.");
+        }
+        else
+        {
+            int[] temp = createArray(num);
+            WriteLine($"The number of numbers greater than zero: {numberCounter(temp)}");
+            WriteLine();
+        }
+    }
+    else
+    {
+        WriteLine("Invalid input.");
+    }
+}
