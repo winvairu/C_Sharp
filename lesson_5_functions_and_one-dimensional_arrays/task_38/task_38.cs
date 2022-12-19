947efa07ec57fe11069a612c65cf1b0112f8b43d
winvairu <winvairu@outlook.com>
2022-12-19 10:42:01 +0300

    add task 38

---
+++ lesson_5_functions_and_one-dimensional_arrays/task_38/task_38.cs
@@ -0,0 +1,58 @@
+﻿/* Set an array of real numbers. Find the difference between the maximum and
+minimum array elements. */
+
+using static System.Console;
+
+void createArray(int a)
+{
+    double[] array = new double[a];
+    for (int i = 0; a > 0; a--, i++)
+    {
+        array[i] = new Random().Next(1000, 100000) / 1000.0;
+        Write(array[i]);
+        Write(" ");
+    }
+    WriteLine();
+    double min = array[0];
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min) 
+        {
+            min = array[i];
+        }
+    }
+    double s = max - min;
+    WriteLine("MAX - MIN = {0:0.000}", s);
+}
+
+int num;
+while (true)
+{
+    Write("Enter the number of array elements or \"0\" to complete: ");
+    if (int.TryParse(ReadLine(), out num))
+    {
+        if (num == 0)
+        {
+            WriteLine("End");
+            break;
+        }
+        else if (num < 0)
+        {
+            WriteLine("Invalid input.");
+        }
+        else
+        {
+            createArray(num);
+            WriteLine();
+        }
+    }
+    else
+    {
+        WriteLine("Invalid input.");
+    }
+}
