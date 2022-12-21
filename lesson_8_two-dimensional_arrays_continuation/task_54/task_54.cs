3f45838d4e446df1ab9ef34cda057708925fc828
winvairu <winvairu@outlook.com>
2022-12-21 16:31:54 +0300

    add task 54

---
+++ lesson_8_two-dimensional_arrays_continuation/task_54/task_54.cs
@@ -0,0 +1,93 @@
+﻿/* Set a two-dimensional array.
+Write a program that arranges the elements of each row of a
+two-dimensional array in descending order. */
+
+using static System.Console;
+
+int inputValidation()
+{
+    int a = 0;
+    while (true)
+    {
+        if (int.TryParse(ReadLine(), out a))
+        {
+            if (a == 0)
+            {
+                WriteLine("End");
+                Environment.Exit(0);
+            }
+            else if (a < 0)
+            {
+                WriteLine("Invalid value. Try again or enter \"0\" to exit:");
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            WriteLine("Invalid value. Try again or enter \"0\" to exit:");
+        }
+    }
+    return a;
+}
+
+int[,] createArray(int a, int b)
+{
+    int[,] matrix = new int[a, b];
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            matrix[i, j] = new Random().Next(-99, 100);
+        }
+    }
+    return matrix;
+}
+
+void printArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Write("{0,4:}", matrix[i, j]);
+        }
+        WriteLine();
+    }
+    WriteLine();
+}
+
+int[,] createNewArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = matrix.GetLength(1) - 1; j >= 0; j--)
+        {
+            for (int k = j; k >= 0; k--)
+            {
+                if (matrix[i, j] <= matrix[i, k])
+                {
+                    continue;
+                }
+                else
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+WriteLine("Enter the number of rows of the two-dimensional array or enter \"0\" to exit:");
+int row = inputValidation();
+WriteLine("Enter the number of columns of the two-dimensional array or enter \"0\" to exit:");
+int column = inputValidation();
+int[,] twoDimensional = createArray(row, column);
+printArray(twoDimensional);
+int[,] newTwoDimensional = createNewArray(twoDimensional);
+printArray(newTwoDimensional);
