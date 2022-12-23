b536e8c62ee4e8b4d9bb36b1d64bc7aff8d94e69
winvairu <winvairu@outlook.com>
2022-12-23 19:16:07 +0300

    add task 64

---
+++ lesson_9_recursion/task_64/task_64.cs
@@ -0,0 +1,50 @@
+﻿/* Set the value to N. Write a program that outputs
+all natural numbers in the range from N to 1. Execute using recursion. */
+
+using static System.Console;
+
+int inputValidation()
+{
+    int a = 0;
+    while (true)
+    {
+        if (int.TryParse(ReadLine(), out a))
+        {
+            if (a == 0)
+            {
+                WriteLine("End");
+                Environment.Exit(0);
+            }
+            else if (a < 0 || a > 50)
+            {
+                WriteLine("Invalid value. Try again or enter \"0\" to exit:");
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            WriteLine("Invalid value. Try again or enter \"0\" to exit:");
+        }
+    }
+    return a;
+}
+
+string numberRec(int a, int b)
+{
+    if ( a <= b)
+    {
+        return numberRec(a + 1, b) + $"{a} ";
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+
+int number1 = 1;
+WriteLine("Enter an unsigned integer (from 1 to 50) or enter \"0\" to exit:");
+int number2 = inputValidation();
+WriteLine(numberRec(number1, number2));
